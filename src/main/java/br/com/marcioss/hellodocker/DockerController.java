5f4b4a247c5acb25e09a0e4e415f6e2ed4c306a2
Marcio Santos <marcioadsworks@gmail.com>
2022-09-02 08:41:46 -0300

    add log info

--- src/main/java/br/com/marcioss/hellodocker/DockerController.java
+++ src/main/java/br/com/marcioss/hellodocker/DockerController.java
@@ -1,18 +1,22 @@
 package br.com.marcioss.hellodocker;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DockerController {
 
+    Logger logger = LoggerFactory.getLogger(DockerController.class);
     @RequestMapping("/hello-docker")
     public HelloDocker greeting(){
 
+        logger.info("Endpoint /hello docker was called!!");
 //        var hostName=  System.getenv("COMPUTERNAME");
         var hostName=  System.getenv("HOSTNAME");
 
-        return new HelloDocker("Hello DOcker", hostName);
+        return new HelloDocker("Hello Docker", hostName);
     }
 
 
